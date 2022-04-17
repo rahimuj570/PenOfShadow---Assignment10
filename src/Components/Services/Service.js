984e3937fc7f70fa14e4656ccc3bd719727cfb8e
rahimuj570 <rahimuj570@gmail.com>
2022-04-17 17:02:27 +0600

    service data is fetched in services

--- src/Components/Services/Service.js
+++ src/Components/Services/Service.js
@@ -1,31 +1,28 @@
 import React from "react";
 
-const Service = () => {
+const Service = ({ service }) => {
+  const { _id, picture, price, name, about } = service;
+
   return (
     <div className="my-3 rounded-lg shadow mx-2 hover:shadow-lg duration-300 p-2 ">
-      <div className="w-4/6 mx-auto mt-2 mb-4">
-        <img
-          className=" w-full  "
-          src="chrome://branding/content/about-logo.png"
-          alt=""
-        />
+      <div className="w-6/6 h-60 mx-auto mt-2 mb-4">
+        <img className=" w-full h-full  " src={picture} alt="" />
       </div>
       <div className="hover:scale-105 duration-300 shadow px-2 py-4 mx-1 my-2 flex justify-between flex-col">
         <div className="font-poppins">
           <p className="my-1">
             <span className="text-base font-semibold text-md ">Name:</span>{" "}
-            Tesyfy
+            {name}
           </p>
           <p className="my-1">
-            <span className="text-base font-semibold text-md ">Price:</span> $10
+            <span className="text-base font-semibold text-md ">Price:</span> $
+            {price + "/hr"}
           </p>
           <p className="my-2">
             <span className="text-base font-semibold text-md ">
               Short Description :
             </span>{" "}
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illo totam
-            delectus beatae quis velit neque autem corrupti, in quisquam
-            incidunt.
+            {about}
           </p>
         </div>
         <div className="mt-4">
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Service from "./Service";
 
 const Services = () => {
+  const [data, setData] = useState();
+  useEffect(() => {
+    fetch("FakeData/data.json")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, []);
+
   return (
     <div id="services" className="my-16">
       <div className="w-4/6 md:w-3/6 mx-auto">
@@ -9,12 +16,14 @@ const Services = () => {
           Services
         </h1>
         <p className="text-base mt-1 font-exo text-center">
-          I'm a Ghost Writer, Who Write for Other But Always be Anonymous.
+          I'm a Ghost Writer, Who Write for Others But Always be Anonymous.
         </p>
       </div>
       <div className="mt-5 mx-20">
         <div className=" grid md:grid-cols-3 grid-cols-1 sm:grid-cols-2  justify-center ">
-          <Service />
+          {data?.map((service) => (
+            <Service key={service._id} service={service} />
+          ))}
         </div>
       </div>
     </div>
