3bc92702dd1e6c450b7e0f2dd18c31458fbdd46f
rahimuj570 <rahimuj570@gmail.com>
2022-04-18 23:08:03 +0600

    blog and about page is ready

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="mt-10 w-4/6 md:w-3/6 mx-auto">
+        <h1 className="border-red-500 mx-auto rounded pb-1 border-b-4 font-exo text-4xl text-center">
+          About Me
+        </h1>
+        <p className="text-base mt-1 font-exo text-center">
+          My Goal of This Year.
+        </p>
+      </div>
+      <div className="shadow-lg m-10 p-5">
+        <div className=" w-3/6 sm:w-1/6 mx-auto  rounded-full overflow-hidden">
+          <img className=" w-full" src="me.jpg" alt="" />
+        </div>
+        <div className="text-center mx-6 rounded p-2 mt-5 shadow font-exo text-base">
+          My Goal of 2022 is become an enough Good MERN developer. <br />
+          Then I'll do a Job and bare my University fees and gift my Mom and Dad
+          every month. <br />
+          Then, if I'll get a chance to help poor peoples, I will help them if
+          avail. <br />
+          And at the end I'll always try to learn new thing as now.
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default About;
--- src/Components/Blogs.js
+++ src/Components/Blogs.js
@@ -1,7 +1,99 @@
 import React from "react";
+import { BsQuestionOctagonFill } from "react-icons/bs";
+import { HashLink } from "react-router-hash-link";
+import { FaArrowAltCircleUp } from "react-icons/fa";
+import "animate.css";
+import { HideScroll } from "react-hide-on-scroll";
 
 const Blogs = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="mt-10 w-4/6 md:w-3/6 mx-auto">
+        <h1 className="border-red-500 mx-auto rounded pb-1 border-b-4 font-exo text-4xl text-center">
+          Blogs
+        </h1>
+        <p className="text-base mt-1 font-exo text-center">
+          Answers of Three Question
+        </p>
+      </div>
+      <div className="my-10 mx-5 sm:mx-10">
+        <div className="flex items-center">
+          <BsQuestionOctagonFill className="basis-20  text-3xl text-red-500" />{" "}
+          <h1 className="text-xl font-bold">
+            What is Difference between authorization and authentication?
+          </h1>
+        </div>
+        <div className="mt-2 ml-10 text-base">
+          <p>Authorization and Authentication both are the security terms.</p>
+          <p>
+            By Authorization or Authentication we can access private Recourses
+            of a platform.{" "}
+          </p>
+          <p>
+            In Authentication we need to provide our Email, Password and other
+            Data to prove that I'm the right and legal person to access or use
+            the system.
+          </p>
+          <p>
+            On the otherhand in Authorization, we need to just provide our
+            different account details such as Google, Facebook, Twitter, etc. to
+            prove that We are the right person to access the system.
+          </p>
+          <p>
+            Actually in Authorization, User verified by third-party application.
+          </p>
+        </div>
+
+        <div className="mt-5 flex items-center">
+          <BsQuestionOctagonFill className="basis-20  text-3xl text-red-500" />{" "}
+          <h1 className="text-xl font-bold">
+            Why are you using Firebase and What other options do you have to
+            implement authentication?
+          </h1>
+        </div>
+        <div className="mt-2 ml-10 text-base">
+          <p>Firebase is a product of Google.</p>
+          <p>
+            Generally I'm using Firebase for Authentication/Authorization
+            services.
+          </p>
+          <p>
+            I believe that, The third-party platform can better handle this
+            sensitive section than me.
+          </p>
+          <p>
+            Because The third-party platforms have enough professional developer
+            and programmer.
+          </p>
+          <p>
+            In some practice project I'm using free hosting service of Firebase.
+          </p>
+          <p>
+            Okta, AuthO, Passport, Amazon Cognito, Centrify, OneLogin, Heroku
+            etc. are the alternative of Firebase.
+          </p>
+        </div>
+        <div className="mt-5 flex items-center">
+          <BsQuestionOctagonFill className="basis-20  text-3xl text-red-500" />{" "}
+          <h1 className="text-xl font-bold">
+            What services does firebase provide other than Authentication
+          </h1>
+        </div>
+        <div className="mt-2 ml-10 text-base">
+          <p>Firebase provide free and paid hosting service.</p>
+          <p>Fire base Provide Cloud Storage.</p>
+          <p>It also provide Google Analytics.</p>
+          <p>You can use Cloud Firestore and Cloud Functions.</p>
+          <p>You can use Realtime Database via Firebase</p>
+        </div>
+      </div>
+      <HideScroll variant="down">
+        <HashLink to="#top">
+          <FaArrowAltCircleUp className="animate__animated animate__bounce animate__infinite animate__slower animate__delay-1s sticky bottom-12 float-right right-10 opacity-10 hover:opacity-100 duration-200 hover:text-red-500 text-5xl " />
+        </HashLink>
+      </HideScroll>
+    </>
+  );
 };
 
 export default Blogs;
